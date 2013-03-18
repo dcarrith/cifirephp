1dc0c0429aa57dc90c0579580f3b5a537320f225
root <root@snorbuntu.(none)>
2013-03-18 11:06:29 -0400

    Added the facade and service provider.

---
+++ src/Dcarrith/CIFirePHP/CIFirePHPFacade.php
@@ -0,0 +1,13 @@
+<?php namespace DCarrith\CIFirePHP;
+
+use Illuminate\Support\Facades\Facade;
+
+class CIFirePHPFacade extends Facade {
+
+        /**
+         * Get the registered name of the component.
+         *
+         * @return string
+         */
+        protected static function getFacadeAccessor() { return 'cifirephp'; }
+}
